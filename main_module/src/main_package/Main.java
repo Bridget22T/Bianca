c84fb5c726fb9b3e04bc6b0e7754fe8bceaed942
Bridget22T <tukundanebridget2@gmail.com>
2025-03-13 09:24:09 -0700

    Commit02
    In NV on line 7, Mukiibi Nelly was changed to Hakim Ahmed

--- main_module/src/main_package/Main.java
+++ main_module/src/main_package/Main.java
@@ -4,7 +4,7 @@ import student_package.Student;
 
 public class Main {
     public static void main(String[] args) {
-        Student s1 = new Student("Mukiibi Nelly", 25);
+        Student s1 = new Student("Hakim Ahmed", 25);
         s1.displayIfo();
 
         Lecturer lec=new Lecturer(" Jane",40);
