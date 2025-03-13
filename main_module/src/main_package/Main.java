fe1c23026659b7b6863cf235870d3e5a1e856a24
Bridget22T <tukundanebridget2@gmail.com>
2025-03-13 09:30:14 -0700

    Commit02
    In NV on line 10,40 was changed to 35

--- main_module/src/main_package/Main.java
+++ main_module/src/main_package/Main.java
@@ -7,7 +7,7 @@ public class Main {
         Student s1 = new Student("Hakim Ahmed", 25);
         s1.displayIfo();
 
-        Lecturer lec=new Lecturer(" Shamim",40);
+        Lecturer lec=new Lecturer(" Shamim",35);
         lec.displayIfo();
         }
     }
