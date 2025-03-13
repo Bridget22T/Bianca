bc9866622477719e16b5ea61257dddaa1448f90c
Bridget22T <tukundanebridget2@gmail.com>
2025-03-13 09:17:40 -0700

    commit01
    In NV,on line 7 8 was changed to 5

--- main_module/src/main_package/Main.java
+++ main_module/src/main_package/Main.java
@@ -4,7 +4,7 @@ import student_package.Student;
 
 public class Main {
     public static void main(String[] args) {
-        Student s1 = new Student("Mukiibi Nelly", 28);
+        Student s1 = new Student("Mukiibi Nelly", 25);
         s1.displayIfo();
 
         Lecturer lec=new Lecturer("Kagugube Mike",40);
